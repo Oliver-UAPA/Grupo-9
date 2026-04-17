9ef5dbc97c3e383adbc967b5fde453808db700c8
Oliver-UAPA <oliverleo@uapa.edu.do>
2026-04-17 10:26:17 -0400

    Update login.php

--- login.php
+++ login.php
@@ -38,6 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión – Grupo 9</title>
